46b7c16a976683b26082d0027abe73eedfdbb28a
Carlos Cruz <ccruzortega@gmail.com>
2020-05-20 21:43:58 -0500

    Conexion a API finalizada

--- app/src/main/java/com/arthur/retosmart/Dashboard.java
+++ app/src/main/java/com/arthur/retosmart/Dashboard.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +18,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Dashboard extends AppCompatActivity {
 
     // VARIABLES DE FIREBASE
@@ -25,6 +38,12 @@ public class Dashboard extends AppCompatActivity {
     // VARIABLES DE INTERFAZ
     private TextView nombre;
     private TextView correo;
+    private  TextView nuevosCasos;
+    private TextView totalCasos;
+    private TextView nuevosCurados;
+    private TextView totalCurados;
+    private TextView nuevasMuertes;
+    private TextView totalMuertes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +54,10 @@ public class Dashboard extends AppCompatActivity {
         mDatadatabase= FirebaseDatabase.getInstance().getReference();
         nombre = (TextView) findViewById(R.id.tv_NombreUsuario);
         correo = (TextView) findViewById(R.id.tv_CorreoUsuario);
+        inicializarVariables();
         informacionDeUsuario();
+        obtenerDatosDeApi();
+
     }
 
     public void cerrarSesion(View view){
@@ -63,5 +85,77 @@ public class Dashboard extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void obtenerDatosDeApi(){
+        String sql = "https://api.covid19api.com/summary";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        URL url = null;
+        HttpURLConnection con;
+        try {
+            url=new URL(sql);
+            con =( HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String inputLIne;
+            StringBuffer response = new StringBuffer();
+            String json = "";
+            while ((inputLIne = in.readLine()) !=null){
+                response.append(inputLIne);
+            }
+
+            json = response.toString();
+            System.out.println(json);
+            JSONObject jsonObject = new JSONObject(json);
+            String global = jsonObject.getString("Global");
+            jsonObject = new JSONObject(global);
+
+            // CASOS
+            asignarValorDeApi(nuevosCasos,jsonObject.getString("NewConfirmed"));
+            asignarValorDeApi(totalCasos,jsonObject.getString("TotalConfirmed"));
+
+            // CURADOS
+            asignarValorDeApi(nuevosCurados, jsonObject.getString("NewRecovered"));
+            asignarValorDeApi(totalCurados, jsonObject.getString("TotalRecovered"));
+
+            // MUERTES
+            asignarValorDeApi(nuevasMuertes, jsonObject.getString("NewDeaths"));
+            asignarValorDeApi(totalMuertes, jsonObject.getString("TotalDeaths"));
+          //  System.out.println("INFORMACION RECOPILADA: " + jsonObject.getString("NewConfirmed"));
+            /*JSONArray jsonar=null;
+
+            jsonar = new JSONArray(json);
+
+            for(int i = 0 ; i < jsonar.length(); i ++){
+                JSONObject jsonObject = jsonar.getJSONObject(i);
+                Log.d("SLD", "obtenerDatosDeApi: " +jsonObject.optString("Global"));
+            }*/
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void asignarValorDeApi(TextView x, String texto){
+        x.setText(x.getText().toString() +" "+ texto);
+    }
+
+    public void inicializarVariables(){
+        nuevosCasos = (TextView) findViewById(R.id.Nuevos_Casos);
+        totalCasos = (TextView) findViewById(R.id.Total_Casos);
+        nuevosCurados = (TextView) findViewById(R.id.Nuevos_Curados);
+        totalCurados = (TextView) findViewById(R.id.Total_Curados);
+        nuevasMuertes = (TextView) findViewById(R.id.Nuevas_Muertes);
+        totalMuertes = (TextView) findViewById(R.id.Total_Muertes);
     }
 }
